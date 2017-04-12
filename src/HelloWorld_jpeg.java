68de7a01f7867348afb04e93efcbfa4bee8d3fb7
Glen5641 <cglenn1995@ou.edu>
2017-04-11 20:46:06 -0500

    ?

--- src/HelloWorld_jpeg.java
+++ src/HelloWorld_jpeg.java
@@ -2,7 +2,7 @@
 public class HelloWorld_jpeg {
 
 	public static void main(String[] args) {
-		System.out.println("do i look like i know what a jpeg is");
+		System.out.println("do i look like i know what a jpeg is?");
 	}
 
 }
