7ae9881cf6d801c80beea591f1b195084a7a5504
fox5759 <fox5759@LAB-COUCH-03.sooner.net.ou.edu>
2017-05-03 23:26:49 -0500

    nutin much

--- src/NoozDriver.java
+++ src/NoozDriver.java
@@ -6,6 +6,7 @@ public class NoozDriver {
 	static private NewsController newsController;
 
 	public static void main(String[] args) {
+		
 		//Create the M, V, and C
 		newsDataBaseModel = new NewsDataBaseModel();
 		selectionView = new SelectionView();
